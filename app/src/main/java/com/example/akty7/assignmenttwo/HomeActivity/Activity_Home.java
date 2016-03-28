9d4252336ba92353ce728c83515e3cbdc2be8880
karandwivedi42 <karan.dwivedi42@gmail.com>
2016-03-29 00:07:27 +0530

    Home Activity

--- app/src/main/java/com/example/akty7/assignmenttwo/HomeActivity/Activity_Home.java
+++ app/src/main/java/com/example/akty7/assignmenttwo/HomeActivity/Activity_Home.java
@@ -1,14 +1,152 @@
 package com.example.akty7.assignmenttwo.HomeActivity;
 
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.akty7.assignmenttwo.Activity_Login;
+import com.example.akty7.assignmenttwo.JSONParser;
 import com.example.akty7.assignmenttwo.R;
 
-public class Activity_Home extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Activity_Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private ViewPager viewPager;
+    private Bundle bundle;
+    private TabLayout tabLayout;
+    private JSONParser jp;
+    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        jp = new JSONParser(this);
+        context = this;
+        bundle = getIntent().getExtras();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_home);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("KARAN");
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout_home);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view_home);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager_home);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs_home);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new BlankFragment(), "MY COMPLAINTS");
+        adapter.addFragment(new BlankFragment(), "HOSTEL");
+        adapter.addFragment(new BlankFragment(), "INSTITUTE");
+        viewPager.setAdapter(adapter);
+    }
+
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout_home);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_logout) {
+           // Boolean logoutSuccess = jp.logout().get(0).isSuccessful;
+            Boolean logoutSuccess = Math.random() < 0.5;
+            if(logoutSuccess){
+                startActivity(new Intent(Activity_Home.this, Activity_Login.class));
+                Activity_Home.this.finish();
+            }
+            else {
+                Snackbar.make(findViewById(R.id.drawer_layout_home), "Logout Failed", Snackbar.LENGTH_SHORT)
+                        .setAction("Action", null).show();
+            }
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout_home);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
 }
