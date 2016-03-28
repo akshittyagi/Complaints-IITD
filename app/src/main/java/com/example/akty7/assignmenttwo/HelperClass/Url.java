f85d199c9ae97be577de5f4c9dbeb17a7b6fbe03
Akshit Tyagi <akty.tyagi7@gmail.com>
2016-03-28 20:42:33 +0530

    API URLs

---
+++ app/src/main/java/com/example/akty7/assignmenttwo/HelperClass/Url.java
@@ -0,0 +1,23 @@
+package com.example.akty7.assignmenttwo.HelperClass;
+
+/**
+ * Created by akty7 on 28-Mar-16.
+ */
+public class Url {
+
+    public String main;
+    public String login;
+    public String logout;
+    public String passReset;
+    public String newComplaint;
+    public String hostelComplaints;
+    public String instituteComplaints;
+    public String individualComplaints;
+    public String specificComplaint;
+    public String allComments;
+    public String addComment;
+    public String upvote;
+    public String downvote;
+    public String addUser;
+
+}
--- app/src/main/java/com/example/akty7/assignmenttwo/JSONParser.java
+++ app/src/main/java/com/example/akty7/assignmenttwo/JSONParser.java
@@ -1,7 +1,23 @@
 package com.example.akty7.assignmenttwo;
 
+import com.example.akty7.assignmenttwo.HelperClass.Admin;
+import com.example.akty7.assignmenttwo.HelperClass.Comment;
+import com.example.akty7.assignmenttwo.HelperClass.Complaint;
+import com.example.akty7.assignmenttwo.HelperClass.Url;
+import com.example.akty7.assignmenttwo.HelperClass.UserIn;
+
+import java.util.ArrayList;
+
 /**
  * Created by akty7 on 28-Mar-16.
  */
 public class JSONParser {
+
+    ArrayList<Admin> retAdmins;
+    ArrayList<Comment> retComments;
+    ArrayList<Complaint> retComplaints;
+    ArrayList<UserIn> retUsers;
+    String url;
+
+    
 }
