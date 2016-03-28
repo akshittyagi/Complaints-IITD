990c43227348e617f90371b1108abeb6e4f2921f
karandwivedi42 <karan.dwivedi42@gmail.com>
2016-03-28 21:27:46 +0530

    Basic Login UI

--- app/src/main/java/com/example/akty7/assignmenttwo/LoginActivity.java
+++ app/src/main/java/com/example/akty7/assignmenttwo/LoginActivity.java
@@ -2,6 +2,8 @@ package com.example.akty7.assignmenttwo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+
+
     }
 }
--- app/src/main/java/com/example/akty7/assignmenttwo/MainActivity.java
+++ app/src/main/java/com/example/akty7/assignmenttwo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.akty7.assignmenttwo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -26,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        startActivity(new Intent(this,LoginActivity.class));
+
     }
 
     @Override
