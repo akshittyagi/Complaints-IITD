8b76c3ddedf55015ff2e8a02eb128899a66b223b
karandwivedi42 <karan.dwivedi42@gmail.com>
2016-03-29 03:56:36 +0530

    Complaint

---
+++ app/src/main/java/com/example/akty7/assignmenttwo/HomeActivity/ComplaintRecyclerViewAdapter.java
@@ -0,0 +1,97 @@
+package com.example.akty7.assignmenttwo.HomeActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.akty7.assignmenttwo.HelperClass.Complaint;
+import com.example.akty7.assignmenttwo.JSONParser;
+import com.example.akty7.assignmenttwo.R;
+
+import java.util.ArrayList;
+
+public class ComplaintRecyclerViewAdapter extends RecyclerView.Adapter<ComplaintRecyclerViewAdapter.DataObjectHolder>{
+
+    ArrayList<Complaint> mDataset;
+    JSONParser jp;
+    Context context;
+    public ComplaintRecyclerViewAdapter(ArrayList<Complaint> usercomps) {
+        jp = new JSONParser(context);
+        mDataset = usercomps;
+    }
+
+    @Override
+    public DataObjectHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_complaint, parent, false);
+
+        DataObjectHolder dataObjectHolder = new DataObjectHolder(view);
+        return dataObjectHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(DataObjectHolder holder, int position) {
+
+        final Complaint comp = mDataset.get(position);
+
+        holder.creator.setText(jp.getUser(comp.filedByUserId).name);
+        if(comp.complaintstatus.equals("true")) holder.status.setImageResource(R.id.tick);
+        else holder.status.setImageResource(R.id.cross);
+        holder.date.setText(comp.createdat);
+        holder.vote.setHint("[" + comp.upvotes + "/" + comp.downvotes + "]");
+        holder.v.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context,Activity_Complaint.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("complaintid",comp.compId);
+                intent.putExtras(bundle);
+                context.startActivity(intent);
+            }
+
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+    public void addItem(Complaint dataObj, int index) {
+        mDataset.add(index, dataObj);
+        notifyItemInserted(index);
+    }
+
+    public void deleteItem(int index) {
+        mDataset.remove(index);
+        notifyItemRemoved(index);
+    }
+
+    public static class DataObjectHolder extends RecyclerView.ViewHolder {
+
+        View v;
+        TextView title;
+        TextView creator;
+        TextView date;
+        ImageView status;
+        TextView vote;
+        public DataObjectHolder(View itemView) {
+            super(itemView);
+            v= itemView;
+            title = (TextView) itemView.findViewById(R.id.comp_card_title);
+            creator = (TextView) itemView.findViewById(R.id.comp_card_creator);
+            date = (TextView) itemView.findViewById(R.id.comp_card_date);
+            status = (ImageView) itemView.findViewById(R.id.comp_card_image);
+            vote = (TextView) itemView.findViewById(R.id.comp_card_updown);
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/akty7/assignmenttwo/HomeActivity/Fragment_MyActivity.java
@@ -0,0 +1,49 @@
+package com.example.akty7.assignmenttwo.HomeActivity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.akty7.assignmenttwo.DividerItemDecoration;
+import com.example.akty7.assignmenttwo.HelperClass.Complaint;
+import com.example.akty7.assignmenttwo.JSONParser;
+import com.example.akty7.assignmenttwo.R;
+
+import java.util.ArrayList;
+
+public class Fragment_MyActivity extends Fragment {
+
+    JSONParser myac_jp;
+    RecyclerView mRecyclerView;
+    LinearLayoutManager mLayoutManager;
+    ComplaintRecyclerViewAdapter mAdapter;
+
+    public Fragment_MyActivity() {
+        // Required empty public constructor
+        myac_jp = new JSONParser(getContext());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_myactivity, container, false);
+        ArrayList<Complaint> usercomps = myac_jp.listOfPersonalComplaints();
+
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.myactivity_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL_LIST));
+        mLayoutManager = new LinearLayoutManager(rootView.getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new ComplaintRecyclerViewAdapter(usercomps);
+        mRecyclerView.setAdapter(mAdapter);
+        return rootView;
+
+        return rootView;
+    }
+
+}
