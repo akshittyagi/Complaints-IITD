b27314242d050fea8782b468c88ca439c3f273cb
karandwivedi42 <karan.dwivedi42@gmail.com>
2016-03-29 19:07:48 +0530

    Linking to jsonpaRSER

--- app/src/main/java/com/example/akty7/assignmenttwo/AuthorityActivities/Activity_ComplaintsForMe.java
+++ app/src/main/java/com/example/akty7/assignmenttwo/AuthorityActivities/Activity_ComplaintsForMe.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.akty7.assignmenttwo.HelperClass.Complaint;
 import com.example.akty7.assignmenttwo.HomeActivity.ComplaintRecyclerViewAdapter;
@@ -28,5 +30,23 @@ public class Activity_ComplaintsForMe extends AppCompatActivity {
         mRecyclerView.setLayoutManager(mLayoutManager);
         mAdapter = new AuthorityComplaintsRecyclerViewAdapter(new ArrayList<Complaint>());
         mRecyclerView.setAdapter(mAdapter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_complaint, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_logout) {
+            jp.logoutFromComplaintsForMe();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/akty7/assignmenttwo/HomeChildren/Activity_AddComp.java
+++ app/src/main/java/com/example/akty7/assignmenttwo/HomeChildren/Activity_AddComp.java
@@ -1,7 +1,9 @@
 package com.example.akty7.assignmenttwo.HomeChildren;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -18,14 +20,22 @@ public class Activity_AddComp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addcomp);
         jp = new JSONParser(this);
-        Complaint complaint = new Complaint();
-        complaint.title = ((EditText) findViewById(R.id.addcomp_title)).getText().toString();
-        complaint.complaintcategory = String.valueOf(((Spinner) findViewById(R.id.addcomp_categ)).getSelectedItemPosition());
-        complaint.complaintlevel = String.valueOf(((Spinner) findViewById(R.id.addcomp_level)).getSelectedItemPosition());
-        complaint.description = ((EditText) findViewById(R.id.addcomp_desc)).getText().toString();
-        boolean success = jp.newComplaint(complaint);
-        if(success){
 
-        }
+        (findViewById(R.id.addcomp_fab)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Complaint complaint = new Complaint();
+                complaint.title = ((EditText) findViewById(R.id.addcomp_title)).getText().toString();
+                complaint.complaintcategory = String.valueOf(((Spinner) findViewById(R.id.addcomp_categ)).getSelectedItemPosition());
+                complaint.complaintlevel = String.valueOf(((Spinner) findViewById(R.id.addcomp_level)).getSelectedItemPosition());
+                complaint.description = ((EditText) findViewById(R.id.addcomp_desc)).getText().toString();
+                boolean success = jp.newComplaint(complaint);
+                if(success){
+
+                }
+            }
+        });
+
+
     }
 }
