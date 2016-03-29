ff35e034d6628908aafd09d492a79f31fb52355d
karandwivedi42 <karan.dwivedi42@gmail.com>
2016-03-29 16:26:14 +0530

    Layouts changed

---
+++ app/src/main/java/com/example/akty7/assignmenttwo/AdminActivities/UserManagement.java
@@ -0,0 +1,15 @@
+package com.example.akty7.assignmenttwo.AdminActivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.akty7.assignmenttwo.R;
+
+public class UserManagement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_management);
+    }
+}
